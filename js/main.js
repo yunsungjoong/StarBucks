3e91e71465ca59d261d65a105c13e697a14e3d5e
Y-SungJoong <yun_sj94@naver.com>
2022-11-19 18:43:55 +0900

    추가

--- js/main.js
+++ js/main.js
@@ -95,3 +95,14 @@ function floatingObject(selector, delay, size) {
 floatingObject('.floating1',1,15);
 floatingObject('.floating2',.5,15);
 floatingObject('.floating3',1.5,20);
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {   // forEach는 메서드를 써서 각각의 요소들을 반복한다.
+    new ScrollMagic
+    .Scene({
+        triggerElement: spyEl,  // 보여짐 여부를 감시할 요소를 지정
+        triggerHook : .8  //
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller()); 
+});
